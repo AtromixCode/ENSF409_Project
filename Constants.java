8bb74c4f4b540a971daefc666cfec6570e431138
AtromixCode <AtromixCode@users.noreply.github.com>
2019-03-26 17:05:50 -0600

    removed all the references to a package that is not present.

--- Constants.java
+++ Constants.java
@@ -1,4 +1,4 @@
-package com.company;
+
 
 public interface Constants {
     static final int THRESHOLD = 40;
--- OrderLine.java
+++ OrderLine.java
@@ -1,4 +1,4 @@
-package com.company;
+
 
 public class OrderLine {
     private Item item;
