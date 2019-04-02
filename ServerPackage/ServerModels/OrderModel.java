5a209eb4551f72d604bcc0a8fdb9107761b48bba
AtromixCode <AtromixCode@users.noreply.github.com>
2019-04-02 10:47:44 -0600

    Initial deleting

--- ServerPackage/ServerModels/OrderModel.java
+++ ServerPackage/ServerModels/OrderModel.java
@@ -21,34 +21,8 @@ public class OrderModel {
     String dateString;
     private int orderID;
 
-    public OrderModel(File file)
-    {
-        orderFile = file;
-        orderLines = new ArrayList<OrderLineModel>();
-        date = new Date ();
-        format = new SimpleDateFormat("MMMM dd, yyyy");
-        dateString = format.format(date);
-        orderID = (int)(Math.random() * 90000) + 10000;
-    }
+    public OrderModel(){
 
-
-    public void addLine(ItemModel item)
-    {
-        OrderLineModel ol = new OrderLineModel(item);
-        orderLines.add(ol);
-    }
-
-    public void createOrder(boolean newLine) throws FileNotFoundException
-    {
-        fileWrite = new PrintWriter(new FileOutputStream(orderFile, true));
-        if (newLine) {
-            fileWrite.println("**********************************************************************");
-            fileWrite.println("ORDER ID:\t\t" + orderID + "\r\n" +
-                              "Date Ordered:\t\t" + dateString);
-        }
-        for (OrderLineModel o: orderLines)
-            fileWrite.println(o);
-        fileWrite.close();
     }
 
     public String getDateString(){return dateString;}
